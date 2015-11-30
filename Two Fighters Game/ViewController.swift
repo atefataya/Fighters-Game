4083f01ff8f9ada0ac474aee7a2e017848331228
Atef H Ataya <atef.ataya@yahoo.com>
2015-11-30 14:12:04 -0800

    Add the restart function

--- Two Fighters Game/ViewController.swift
+++ Two Fighters Game/ViewController.swift
@@ -16,57 +16,88 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var outputLabel: UILabel!
     
+    @IBOutlet weak var restartButton: UIButton!
+    
     var player: Character!
     var enemy: Character!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-//        
+       
+        initialize()
+        
+    }
+
+    func initialize(){
+        self.restartButton.enabled = false
+        self.restartButton.hidden = true
+        
         let hpPlayer = Int(arc4random_uniform(100))
         let hpEnemy = Int(arc4random_uniform(100))
         
         let powerPlayer = Int(arc4random_uniform(30))
         let powerEnemy = Int(arc4random_uniform(30))
-
         
-    
+        
+        
         player = Character(startingHp: hpPlayer, attackPwr: powerPlayer, playerName: "Player")
         enemy = Character(startingHp: hpEnemy, attackPwr: powerEnemy, playerName: "Enemy")
         
-        print (player.hp)
-        print(enemy.hp)
-        print(player.attackPwr)
-        print(enemy.attackPwr)
-        
+        self.playerButton.enabled = true
+        self.enemyButton.enabled = true
+        self.outputLabel.text = "Click Attack to Start"
     }
-
 
     @IBAction func playerAction(sender: AnyObject) {
         
         if enemy.attemptAttack(player.attackPwr){
             
             outputLabel.text = "\(player.name) attack \(enemy.name)"
+            self.playerButton.enabled = false
+            NSTimer.scheduledTimerWithTimeInterval(3, target: self, selector: "enablePlayerButton", userInfo: nil, repeats: false)
         } else {
             outputLabel.text = "Attack was unsuccessful"
         }
         
         if !enemy.isAlive {
             outputLabel.text = "\(player.name) killed \(enemy.name)"
-            
+            self.playerButton.enabled = false
+            self.enemyButton.enabled = false
+            self.outputLabel.text = "Player WON!!!!The End"
+            self.restartButton.hidden = false
+            self.restartButton.enabled = true
+        }
+    }
+ 
+    @IBAction func enemyAction(sender: AnyObject) {
+        
+        if player.attemptAttack(enemy.attackPwr) {
+            outputLabel.text = "\(enemy.name) attack \(player.name)"
+            self.enemyButton.enabled = false
+            NSTimer.scheduledTimerWithTimeInterval(3, target: self, selector: "enableEnemyButton", userInfo: nil, repeats: false)
+        }else {
+            outputLabel.text = "Attack was usuccessful"
         }
         
-        self.playerButton.enabled = false
-        NSTimer.scheduledTimerWithTimeInterval(3, target: self, selector: "enablePlayerButton", userInfo: nil, repeats: false)
-        
+        if !player.isAlive {
+            outputLabel.text = "\(enemy.name) killed \(player.name)"
+            self.playerButton.enabled = false
+            self.enemyButton.enabled = false
+            self.outputLabel.text = "Enemy WON!!!!The End"
+            self.restartButton.hidden = false
+            self.restartButton.enabled = true
+        }
     }
+
+    @IBAction func restartAction(sender: AnyObject) {
+        initialize()
+    }
+    
     func enablePlayerButton () {
         
         if self.playerButton.enabled == false {
             self.playerButton.enabled = true
         }
-        
-        
     }
     
     func enableEnemyButton () {
@@ -75,27 +106,5 @@ class ViewController: UIViewController {
             self.enemyButton.enabled = true
         }
     }
-    @IBAction func enemyAction(sender: AnyObject) {
-        
-        if player.attemptAttack(enemy.attackPwr) {
-            outputLabel.text = "\(enemy.name) attack \(player.name)"
-        }else {
-            outputLabel.text = "Attack was usuccessful"
-        }
-        
-        if !player.isAlive {
-            outputLabel.text = "\(enemy.name) killed \(player.name)"
-        }
-        
-        self.enemyButton.enabled = false
-        NSTimer.scheduledTimerWithTimeInterval(3, target: self, selector: "enableEnemyButton", userInfo: nil, repeats: false)
-
-    }
-    
-    func restartGame(){
-        
-        
-    }
-    
 }
 
